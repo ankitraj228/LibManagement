9f58f78071b5f5af0ca11489ce4eed9293e2bcf3
Ankit raj <106767352+ankitraj228@users.noreply.github.com>
2025-03-28 20:47:36 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Installing Java..."
-apt-get update && apt-get install -y openjdk-17-jdk  # Change to Java 11 or 21 if needed
-java -version  # Verify installation
-mvn clean package  # Build your Java app
